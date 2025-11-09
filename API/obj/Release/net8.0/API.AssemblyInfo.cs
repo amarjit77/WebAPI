ad923ec8124078f7821075cb6d27b3c8299a9e4b
AMAR V. KAMBLE <kamarjit77@gmail.com>
2025-11-10 02:20:24 +0530

    fixed9

--- API/obj/Release/net8.0/API.AssemblyInfo.cs
+++ API/obj/Release/net8.0/API.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ef2fab6ec3b281937bb43adb4c9897fe304c456")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
