bf0d6a3c9710a9fed318ad235183525c1a3699e9
AMAR V. KAMBLE <kamarjit77@gmail.com>
2025-11-10 03:11:49 +0530

    fixed 16

---
+++ API/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        // Addition method for two numbers
+        [HttpGet("add")]
+        public ActionResult<int> Add([FromQuery] int a, [FromQuery] int b)
+        {
+            return a + b;
+        }
+    }
+}
